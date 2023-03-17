50eec319bea991f92fc51440b86211ffbe060003
sigit <sigit.cullen@gmail.com>
2023-03-17 12:22:28 +0700

    menambahkan library cas

---
+++ app/Config/Cas.php
@@ -0,0 +1,5 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+$config['cas_server_url'] = 'http://data.bkd.jambiprov.go.id/';
+$config['phpcas_path'] = 'CAS.php';
+$config['cas_disable_server_validation'] = TRUE;
+$config['cas_debug'] = TRUE; // <--  use this to enable phpCAS debug mode
--- app/Controllers/RestClient.php
+++ app/Controllers/RestClient.php
@@ -2,10 +2,25 @@
 
 namespace App\Controllers;
 
+require_once APPPATH . 'Libraries\CAS.php';
+
+
 class RestClient extends BaseController
 {
-    public function index()
+    // public function index()
+    // {
+    //     $client = \Config\Services::curlrequest();
+    //     $url = "http://data.bkd.jambiprov.go.id/rest-data-pokok/198612172011011003";
+    //     $response = $client->request('GET', $url);
+    //     echo $response->getBody();
+    // }
+
+
+    function index()
     {
-        return view('welcome_message');
+        $this->load->library('CAS');
+        $this->cas->force_auth();
+        $user = $this->cas->user();
+        echo "Hello, $user->userlogin!";
     }
 }
